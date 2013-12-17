5c4437054c237625fc1aea97f6e4f935aad38579
alvinyuan <alvin.yuan@berkeley.edu>
2013-12-16 19:18:23 -0800

    hysterisis on arduino side to smooth out feeling

--- kinect2bioloid/ax12_test/ax12_test.ino
+++ kinect2bioloid/ax12_test/ax12_test.ino
@@ -3,6 +3,13 @@
 AX12 motor;
 int p, id, mode;
 
+// Gesture Mimicking State
+const int numMotors = 6;
+char packet[numMotors + 1];
+const int readingMultiplier = 10;
+int lastReadingSent[numMotors];
+const int hysterisisThreshold = 1;
+const int motorVelocity = 100;
 void setup() {
  
   motor = AX12();
@@ -31,43 +38,27 @@ void setup() {
   id = motor.id;
   mode = 1;
   //motor.writeInfo (ID, 5);
+  for (int i = 0; i < numMotors; i++) {
+    //Initialize all to 50
+    lastReadingSent[i] = 50;
+  }
 }
 
 
 void loop() {
-  while(!Serial.available()) {
+  if (Serial.available()) {
+    Serial.readBytesUntil('\n', packet, numMotors);
+  
+    for (int i = 0; i < numMotors; i++) {
+      char reading = packet[i];
+      if (abs(reading - lastReadingSent[i]) > hysterisisThreshold) {
+        lastReadingSent[i] = reading;
+      }
+    }
   }
-  char packet[6];
-  Serial.readBytesUntil('\n', packet, 6);
-  const int readingMultiplier = 20;
-
-  motor.id = 1;
-  char reading = packet[0];
-  motor.setVel(150);
-  motor.setPos(reading * readingMultiplier);
-  
-  motor.id = 2;
-  reading = packet[1];
-  motor.setVel(150);
-  motor.setPos(reading * readingMultiplier);
-  
-  motor.id = 3; 
-  reading = packet[2];
-  motor.setVel(150);
-  motor.setPos(reading * readingMultiplier);
-
-  motor.id = 4;
-  reading = packet[3];
-  motor.setVel(150);
-  motor.setPos(reading  * readingMultiplier);
-  
-  motor.id = 6;
-  reading = packet[4];
-  motor.setVel(250);
-  motor.setPos(reading * readingMultiplier);
-  
-  // set unused motor to 500
-  motor.id = 5;
-  motor.setVel(150);
-  motor.setPos(500);
+  for (int i = 0; i < numMotors; i++) {
+    motor.id = i+1;
+    motor.setVel(motorVelocity);
+    motor.setPos(lastReadingSent[i] * readingMultiplier);
+  }
 }
